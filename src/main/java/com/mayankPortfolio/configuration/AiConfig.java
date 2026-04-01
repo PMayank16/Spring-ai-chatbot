072d79330169e12469e56b29e8945832d097a756
Mayank Pathak <your-email@example.com>
2026-04-01 16:30:01 +0530

    Fixed port + disabled local DB for Render

--- src/main/java/com/mayankPortfolio/configuration/AiConfig.java
+++ src/main/java/com/mayankPortfolio/configuration/AiConfig.java
@@ -3,8 +3,6 @@ package com.mayankPortfolio.configuration;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.openai.OpenAiChatOptions;
-import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
-import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
